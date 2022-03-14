3600ba219d94e2b9eea6d919b23127d2c54a229f
Leonel90 <97122219+Leonel90@users.noreply.github.com>
2022-03-14 14:52:20 -0500

    html,js
    botón eliminar

--- js/crud.js
+++ js/crud.js
@@ -8,6 +8,33 @@ function limpiar() {
 }
 
 //FUNCIONALIDAD DE LOS BOTONES CON jQUERY
+//Eliminar Registro
+function eliminarRegistro() {
+    $(document).one('click', 'button[type="button"]', function (event) {
+        let id = this.id;
+        var lista = [];
+        $('#listaProductos').each(function () {
+            var celdas = $(this).find('tr.Reg_A' + id);
+            celdas.each(function () {
+                var registro = $(this).find('span.mid');
+                registro.each(function () {
+                    lista.push($(this).html())
+                });
+            });
+        });
+        nuevoId = lista[0].substr(1);
+        alert(nuevoId);
+        db.transaction(function(transaction) {
+            var sql = "DELETE FROM productos WHERE id=" + nuevoId + ";"
+            transaction.executeSql(sql, undefined, function () {
+                alert("Registro Borrado correctamente, Por favor actualice la tabla")
+            }, function (transaction, err) {
+                alert("err.message");
+            })
+        })
+    });
+}
+
 //Boton Crear Tabla de productos
 $(function () {
     $("#crear").click(function () {
@@ -44,7 +71,7 @@ $(function () {
                         var precio = row.precio;
                         $("#listaProductos").append('<tr id="fila' + id + '" class="Reg_A' + id + '"><td><span class="mid">A' +
                             id + '</span></td><td><span>' + item + '</span></td><td><span>' +
-                            precio + '</span></td><td>Editar</td><td>Eliminar</td></tr>');
+                            precio + ' USD$</span></td><td><button type="button" id="A'+id+'" class="btn btn-success"><img src="/img/edit.png"/></button></td><td><button type="button" id="A'+id+'" class="btn btn-danger"onclick="eliminarRegistro()" ><img src="/img/delete.jpg"/></button></td></tr>');
                     }
                 } else {
                     $("#listaProductos").append('<tr><td colspan="5" align="center">No existe registros de productos</td></tr>');
@@ -68,5 +95,19 @@ $(function () {
         })
         limpiar();
         cargarDatos();
+    })
+
+    //Para borrar todos los registros
+    $("#borrarTodo").click(function () {
+        if (!confirm("Está seguro de borrar la tabla?, los datos se perderán permanentemente", ""))
+            return;
+        db.transaction(function (transaction) {
+            var sql = "DROP TABLE productos";
+            transaction.executeSql(sql, undefined, function () {
+                alert("Tabla borrada correctamente, por favor, actualice la página")
+            }, function (transaction, err) {
+                alert(err.message)
+            })
+        })
     })
 })
